b83817778fc68d85b4855b8d92090aa4e32a08f7
tommy-anderson <alexursu@planable.io>
2025-03-24 14:03:22 +0200

    Fix

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,9 +2,9 @@ import { defineConfig, devices } from "@playwright/test";
 
 export default defineConfig({
   testDir: "./tests",
+  testMatch: "**/*.ts",
   fullyParallel: false,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
   reporter: [
     ["list"],
