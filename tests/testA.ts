a179bdd65d40efae915aff0353a106a36d2f2956
tommy-anderson <alexursu@planable.io>
2025-03-24 14:03:46 +0200

    Fix

--- tests/testA.ts
+++ tests/testA.ts
@@ -1,7 +1,7 @@
 import { test, expect } from "@playwright/test";
 
-test("Wait 30 seconds", async ({ page }) => {
+test("Wait 15 seconds", async ({ page }) => {
   await page.goto("https://playwright.dev/");
-  await page.waitForTimeout(30000);
+  await page.waitForTimeout(15000);
   await expect(page).toHaveTitle(/Playwright/);
 });
