24d89718d4d663e238524858389ff46c96b079c9
tommy-anderson <alexursu@planable.io>
2025-03-24 16:16:18 +0200

    👌 Fix

--- tests/specB.ts
+++ tests/specB.ts
@@ -1,19 +1,19 @@
 import { test, expect } from "@playwright/test";
 
 test.describe("specB", () => {
-  test("test1. Wait 10 seconds", async ({ page }) => {
+  test("test1. Wait 100 ms", async ({ page }) => {
     await page.goto("https://playwright.dev/");
-    await page.waitForTimeout(10000);
+    await page.waitForTimeout(100);
     await expect(page).toHaveTitle(/Playwright/);
   });
-  test("test2. Wait 10 seconds", async ({ page }) => {
+  test("test2. Wait 100 ms", async ({ page }) => {
     await page.goto("https://playwright.dev/");
-    await page.waitForTimeout(10000);
+    await page.waitForTimeout(100);
     await expect(page).toHaveTitle(/Playwright/);
   });
-  test("test3. Wait 10 seconds", async ({ page }) => {
+  test("test3. Wait 100 ms", async ({ page }) => {
     await page.goto("https://playwright.dev/");
-    await page.waitForTimeout(10000);
+    await page.waitForTimeout(100);
     await expect(page).toHaveTitle(/Playwright/);
   });
 });
